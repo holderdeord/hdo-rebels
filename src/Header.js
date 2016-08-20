cfe64689ed5dd15b7511138d5d277c2fe3dd5e6b
Jari Bakken <jari@holderdeord.no>
2016-08-20 16:23:57 +0200

    Set up basic HDO React app

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import SharingLinks from './SharingLinks';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="row">
+                    <div className="col-md-2">
+                        <a href="http://www.holderdeord.no">
+                            <div className="hdo-logo">
+                                <strong>Holder de ord</strong>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="col-md-8 text-xs-center">
+                        <h1>Hvem stemmer mot eget parti på Stortinget?</h1>
+                    </div>
+
+                    <div className="col-md-2 text-xs-right">
+                        <SharingLinks />
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+
+import './index.scss';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
